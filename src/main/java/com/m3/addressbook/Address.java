154303e1417217ecdb34289d9a762b004af12b7b
Jordan <jorlee92@gmail.com>
2021-09-09 07:12:54 -0700

    Adding files

---
+++ src/main/java/com/m3/addressbook/Address.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.m3.addressbook;
+
+/**
+ *
+ * @author me
+ */
+public class Address {
+    String address;
+    String owner;
+
+    public Address(String address, String owner){
+        this.address = address;
+        this.owner = owner;
+    }
+}
